98dc32c3f839f19c4ac55e588c46b81e516754a5
anf7503 <56983012+anf7503@users.noreply.github.com>
2022-10-11 21:58:59 -0400

    finished htmlResponses.js for the foreseeable future

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -3,6 +3,9 @@ const fs = require('fs'); // pull in the file system module
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const styles = fs.readFileSync(`${__dirname}/../client/style.css`);
 const js = fs.readFileSync(`${__dirname}/../client/client.js`);
+const form = fs.readFileSync(`${__dirname}/../client/form.html`);
+const formJS = fs.readFileSync(`${__dirname}/../client/form.js`);
+
 
 const serveFile = (response, contentType, file) => {
   response.writeHead(200, { 'Content-Type': contentType });
@@ -15,6 +18,11 @@ const getIndex = (request, response) => {
   serveFile(response, 'text/html', index);
 };
 
+// returns the form html
+const getForm = (request, response) => {
+  serveFile(response, 'text/html', form);
+};
+
 // returns the css styles
 const getStyles = (request, response) => {
   serveFile(response, 'text/css', styles);
@@ -25,8 +33,15 @@ const getJS = (request, response) => {
   serveFile(response, 'application/javascript', js);
 };
 
+// returns the javascript file for the form
+const getFormJS = (request, response) => {
+  serveFile(response, 'application/javascript', formJS);
+};
+
 module.exports = {
   getIndex,
+  getForm,
   getStyles,
-  getJS
+  getJS,
+  getFormJS
 };
